4e10213c936afdb2f7654613da0fcebdf144e9ee
Luka van der Plas <lukavdplas@mailfence.com>
2025-12-04 18:30:16 +0100

    preserve whitespace in annotations

--- frontend/src/app/document/content/annotated-content/annotated-content.component.ts
+++ frontend/src/app/document/content/annotated-content/annotated-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 import { entityKeys } from '@app/models';
 import { entityIcons } from '@app/shared/icons';
 import _ from 'lodash';
@@ -11,6 +11,9 @@ import _ from 'lodash';
 export class AnnotatedContentComponent {
     /** content with annotation in the text */
     @Input({ required: true }) content: string;
+
+    @HostBinding('class') class = 'break-word';
+    @HostBinding('style') style = 'white-space: pre-wrap;'
 
     entityIcons = entityIcons;
 
