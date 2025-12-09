f8d3cf6d5c5e0df7d0d66a74db271861bbe87654
Luka van der Plas <lukavdplas@mailfence.com>
2025-12-09 14:21:34 +0100

    preserve array in snippet pipe

--- frontend/src/app/document/pipes/snippet.pipe.ts
+++ frontend/src/app/document/pipes/snippet.pipe.ts
@@ -11,12 +11,19 @@ export class SnippetPipe implements PipeTransform {
      * @param nCharacters Specifies how many leading characters should be displayed
      */
     transform(text: string | string[], nCharacters=100) {
+        if (!text.length) {
+            return text;
+        }
+
         if (Array.isArray(text)) {
-            text = text.join('\n\n');
+            if (text[0].length >= nCharacters) {
+                return [this.transform(text[0], nCharacters)];
+            }
+            return [text[0], ...this.transform(text.slice(1), nCharacters - text[0].length)];
         }
 
         if (text.length > nCharacters) {
-            const snippedText = text.slice(0, nCharacters).concat('...');
+            const snippedText = text.slice(0, nCharacters).trimEnd().concat('...');
             return snippedText;
         } else {
             return text;
