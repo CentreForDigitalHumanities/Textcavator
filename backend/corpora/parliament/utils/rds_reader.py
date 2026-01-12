cb03cf41d31eae477c329c51782a437ed1e0a361
Luka van der Plas <lukavdplas@mailfence.com>
2026-01-12 15:22:10 +0100

    add rds reader def

---
+++ backend/corpora/parliament/utils/rds_reader.py
@@ -0,0 +1,22 @@
+from typing import Iterable, Dict
+
+import pyreadr
+import pandas
+
+from ianalyzer_readers.readers.core import Reader
+
+
+class RDSReader(Reader):
+    def data_from_file(self, path) -> Iterable[Dict]:
+        result = pyreadr.read_r(path)
+        data: pandas.DataFrame = result['data']
+
+        for _, row in data.iterrows():
+            yield {
+                index: value
+                for index, value in row.items()
+            }
+
+    def iterate_data(self, data: Iterable[Dict], metadata):
+        for row in data:
+            yield {'rows': [row]} # this format is for compatability with the CSV extractor
