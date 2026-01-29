23c6fc08ee2db93762ae3a858c9d629641b63919
Luka van der Plas <lukavdplas@mailfence.com>
2026-01-29 15:38:50 +0100

    add print message

--- iframe-test-app/server.py
+++ iframe-test-app/server.py
@@ -5,4 +5,5 @@ ADDRESS = ('', 9000)
 server = HTTPServer(ADDRESS, SimpleHTTPRequestHandler)
 
 if __name__ == '__main__':
+    print(f'Starting server at http://localhost:{server.server_port}')
     server.serve_forever()
