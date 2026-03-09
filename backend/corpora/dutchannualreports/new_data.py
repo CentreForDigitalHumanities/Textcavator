4be0d514cfa949aa640cb3a61c44a2605b7cbf52
Luka van der Plas <lukavdplas@mailfence.com>
2026-03-09 11:42:39 +0000

    extract relative paths

--- backend/corpora/dutchannualreports/new_data.py
+++ backend/corpora/dutchannualreports/new_data.py
@@ -1,7 +1,7 @@
 import os
 
 from ianalyzer_readers.readers.csv import CSVReader
-from ianalyzer_readers.extract import CSV, Metadata
+from ianalyzer_readers.extract import CSV, Metadata, Order
 from ianalyzer_readers.readers.core import Field
 from django.conf import settings
 
@@ -10,45 +10,65 @@ from corpora.dutchannualreports.pdf_reader import PDFReader, PageText
 class NewDataIndexReader(CSVReader):
     @property
     def data_directory(self):
-        return os.path.join(settings.DUTCHANNUALREPORTS_DATA, 'newdata')
+        return settings.DUTCHANNUALREPORTS_DATA
 
     def sources(self, **kwargs):
-        path = os.path.join(self.data_directory, 'metadata.csv')
+        path = os.path.join(self.data_directory, 'newdata', 'metadata.csv')
         yield path
 
-    fields = [
-        Field(
-            name='path',
-            extractor=CSV('path'),
-        ),
-        Field(
-            name='year',
-            extractor=CSV('year', transform=int),
-        ),
-        Field(
-            name='company',
-            extractor=CSV('company'),
-        )
-    ]
+    def _abs_path(self, path: str) -> str:
+        '''Transform path within newdata subdirectory to absolute path'''
+        return os.path.join(self.data_directory, 'newdata', path)
+
+    def _rel_path(self, path: str) -> str:
+        '''Transform path within newdata subdirectory to one relative to the corpus data
+        directory.'''
+        abs_path = self._abs_path(path)
+        return os.path.relpath(abs_path, self.data_directory)
+
+    @property
+    def fields(self):
+        return [
+            Field(
+                name='abs_path',
+                extractor=CSV('path', transform=self._abs_path),
+            ),
+            Field(
+                name='rel_path',
+                extractor=CSV('path', transform=self._rel_path)
+            ),
+            Field(
+                name='year',
+                extractor=CSV('year', transform=int),
+            ),
+            Field(
+                name='company',
+                extractor=CSV('company'),
+            )
+        ]
 
 class NewDataReader(PDFReader):
     def __init__(self, **kwargs):
         self.index_reader = NewDataIndexReader(**kwargs)
 
-    @property
-    def data_directory(self):
-        return self.index_reader.data_directory
 
     def sources(self, **kwargs):
         docs = self.index_reader.documents(**kwargs)
         for doc in docs:
-            path = os.path.join(self.data_directory, doc['path'])
-            yield path, doc
+            yield doc['abs_path'], doc
 
     fields = [
         Field(
             name='content',
             extractor=PageText()
+        ),
+        Field(
+            name='image_path',
+            extractor=Metadata('rel_path'),
+        ),
+        Field(
+            name='page',
+            extractor=Order(transform=lambda i: i + 1)
         ),
         Field(
             name='company',
