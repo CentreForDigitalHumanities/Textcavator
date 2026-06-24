d2f282fdf73867270fa2363153e9d9e5e1a79158
Luka van der Plas <lukavdplas@mailfence.com>
2026-06-24 09:02:16 +0000

    typing hell

--- frontend/src/app/visualization/wordcloud/wordcloud.component.ts
+++ frontend/src/app/visualization/wordcloud/wordcloud.component.ts
@@ -103,7 +103,7 @@ export class WordcloudComponent implements OnChanges, OnDestroy {
             const options = this.chartOptions(result);
 
             if (this.chart) {
-                this.chart.data = data as any;
+                this.chart.data = data as unknown as any;
                 this.chart.update();
             } else {
                 this.chart = new WordCloudChart('wordcloud', { data, options });
