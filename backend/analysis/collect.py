0f354c21d9d69116b4fc7b36ced9be0b26fbd35f
Luka van der Plas <lukavdplas@mailfence.com>
2026-06-25 10:29:51 +0000

    add collect fn

---
+++ backend/analysis/collect.py
@@ -0,0 +1,35 @@
+from elasticsearch.helpers import scan
+
+from addcorpus.models import Corpus, FieldDisplayTypes
+from es.client import elasticsearch
+from visualization.query import MATCH_ALL
+from visualization.termvectors import request_termvectors_batched, term_counts
+
+
+def content_fields(corpus: Corpus):
+    return [
+        field for field in corpus.configuration.fields
+        if field.display_type == FieldDisplayTypes.TEXT_CONTENT
+    ]
+
+def metadata_fields(corpus: Corpus):
+    return [
+        field for field in corpus.configuration.fields
+        if field.display_type != FieldDisplayTypes.TEXT_CONTENT
+    ]
+
+
+def collect_tokens(corpus: Corpus):
+    client = elasticsearch(corpus)
+    docs = scan(client, MATCH_ALL)
+    # TODO: handle multifields
+    content_field_names = [field.name for field in content_fields(corpus)]
+    meta_field_names = [field.name for field in metadata_fields(corpus)]
+    for hit, vectors in request_termvectors_batched(docs, client, False, content_field_names):
+        metadata = {
+            field: value for field, value in hit['_source'].items()
+            if field in meta_field_names
+        }
+        for field in content_field_names:
+            counts = term_counts(vectors, field)
+            yield field, counts, metadata
