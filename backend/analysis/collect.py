2fd27d419ec1bee75681507c54d176660d8e5d99
Luka van der Plas <lukavdplas@mailfence.com>
2026-06-25 15:07:56 +0000

    handle multifields in collect

--- backend/analysis/collect.py
+++ backend/analysis/collect.py
@@ -8,28 +8,44 @@ from visualization.query import MATCH_ALL
 from visualization.termvectors import request_termvectors_batched, term_counts
 
 
-def content_fields(corpus: Corpus):
-    return corpus.configuration.fields.filter(display_type=FieldDisplayTypes.TEXT_CONTENT)
+def content_fields(corpus: Corpus) -> Iterable[Tuple[str, Optional[str]]]:
+    fields = corpus.configuration.fields.filter(display_type=FieldDisplayTypes.TEXT_CONTENT)
+    for field in fields:
+        yield field.name, None
+        multifields = field.es_mapping.get('fields')
+        if 'clean' in multifields:
+            yield field.name, 'clean'
+        if 'stemmed' in multifields:
+            yield field.name, 'stemmed'
 
-def metadata_fields(corpus: Corpus):
-    return corpus.configuration.fields.exclude(display_type=FieldDisplayTypes.TEXT_CONTENT)
+def content_field_name(name: str, multifield: Optional[str] = None):
+    return f'{name}.{multifield}' if multifield else name
+
+def metadata_fields(corpus: Corpus) -> Iterable[str]:
+    return [
+        field.name for field in
+        corpus.configuration.fields.exclude(display_type=FieldDisplayTypes.TEXT_CONTENT)
+    ]
 
 
-def collect_tokens(corpus: Corpus, index_name: str) -> Iterable[Tuple[str, Dict[str, int], Dict[str, Any]]]:
+def collect_tokens(corpus: Corpus, index_name: str) -> Iterable[Tuple[str, Optional[str], Dict[str, int], Dict[str, Any]]]:
     client = elasticsearch(corpus)
     query = MATCH_ALL | { 'index': index_name, 'allow_no_indices': False }
     docs = scan(client, query)
-    # TODO: handle multifields
-    content_field_names = [field.name for field in content_fields(corpus)]
-    meta_field_names = [field.name for field in metadata_fields(corpus)]
-    for hit, vectors in request_termvectors_batched(docs, client, False, content_field_names):
+    fields = list(content_fields(corpus))
+    field_names = [
+        content_field_name(name, multifield)
+        for name, multifield in fields
+    ]
+    meta_field_names = list(metadata_fields(corpus))
+    for hit, vectors in request_termvectors_batched(docs, client, False, field_names):
         metadata = {
             field: value for field, value in hit['_source'].items()
             if field in meta_field_names
         }
-        for field in content_field_names:
-            counts = term_counts(vectors, field)
-            yield field, counts, metadata
+        for name, multifield in fields:
+            counts = term_counts(vectors, content_field_name(name ,multifield))
+            yield name, multifield, counts, metadata
 
 
 def token_field_name(field_name: str, multifield: Optional[str] = None,  size: int = 1):
@@ -40,8 +56,8 @@ def token_field_name(field_name: str, multifield: Optional[str] = None,  size: i
 
 def token_docs(corpus: Corpus, index_name: str):
     iterator = collect_tokens(corpus, index_name)
-    for field, term_counts, metadata in iterator:
-        token_field = token_field_name(field, None, 1)
+    for field, multifield, term_counts, metadata in iterator:
+        token_field = token_field_name(field, multifield, 1)
         for term, count in term_counts.items():
             for count in range(count):
                 yield { token_field: term } | metadata
--- backend/analysis/tests/test_collect.py
+++ backend/analysis/tests/test_collect.py
@@ -7,11 +7,16 @@ def test_collect_tokens(small_mock_corpus, index_small_mock_corpus):
     corpus = Corpus.objects.get(name=small_mock_corpus)
     index = get_index(small_mock_corpus)
     data = list(collect_tokens(corpus, index))
-    assert len(data) == SPECS['total_docs']
-    assert sum(sum(term_counts.values()) for _, term_counts, _ in data) == SPECS['total_words']
+    assert len(data) == SPECS['total_docs'] * 2 # 2 fields per document (standard + cleaned)
+    assert sum(
+        sum(term_counts.values())
+        for _, multifield, term_counts, _ in data
+        if not multifield
+    ) == SPECS['total_words']
 
-    field, terms, metadata = data[1]
+    field, multifield, terms, metadata = data[2]
     assert field == SPECS['content_field']
+    assert multifield == None
     assert terms['truth'] == 1
     assert terms['a'] == 4
     assert metadata['date'] == '1813-01-28'
@@ -22,11 +27,16 @@ def test_token_docs(small_mock_corpus, index_small_mock_corpus):
     corpus = Corpus.objects.get(name=small_mock_corpus)
     index = get_index(small_mock_corpus)
     data = list(token_docs(corpus, index))
-    assert len(data) == SPECS['total_words']
 
-    singleton = [d for d in data if d['content:1'] == 'alice']
+    assert sum(
+        1 for token in data if token.get('content:1')
+    ) == SPECS['total_words']
+
+    singleton = [d for d in data if d.get('content:1') == 'alice']
     assert len(singleton) == 1
     assert singleton[0]['genre'] == 'Children'
 
-    multiple = [d for d in data if d['content:1'] == 'to']
-    assert len(multiple) == 3
+    assert sum(1 for d in data if d.get('content:clean:1') == 'alice') == 1
+    assert sum(1 for d in data if d.get('content:1') == 'to') == 3
+    assert sum(1 for d in data if d.get('content:clean:1') == 'to') == 0
+
