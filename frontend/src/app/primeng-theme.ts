407ca1fae00c8a6fc76b168bd02bef00f4cef8b8
Luka van der Plas <lukavdplas@mailfence.com>
2026-07-14 15:47:57 +0000

    reconfigure primeng theme for bootstrap

--- frontend/src/app/primeng-theme.ts
+++ frontend/src/app/primeng-theme.ts
@@ -1,33 +1,19 @@
 import { definePreset } from '@primeng/themes';
 import Nora from '@primeng/themes/nora';
 
-const bulmaPrimaryScheme = {
-    50: 'var(--bulma-primary-95)',
-    100: 'var(--bulma-primary-90)',
-    200: 'var(--bulma-primary-80)',
-    300: 'var(--bulma-primary-70)',
-    400: 'var(--bulma-primary-60)',
-    500: 'var(--bulma-primary-50)',
-    600: 'var(--bulma-primary-40)',
-    700: 'var(--bulma-primary-30)',
-    800: 'var(--bulma-primary-20)',
-    900: 'var(--bulma-primary-10)',
-    950: 'var(--bulma-primary-05)',
-}
-
 const graySurfaceScheme = {
-    950: 'var(--bulma-black)',
-    900: 'var(--bulma-black-bis)',
-    800: 'var(--bulma-black-ter)',
-    700: 'var(--bulma-grey-darker)',
-    600: 'var(--bulma-grey-dark)',
-    500: 'var(--bulma-grey)',
-    400: 'var(--bulma-grey)',
-    300: 'var(--bulma-grey-light)',
-    200: 'var(--bulma-grey-lighter)',
-    100: 'var(--bulma-white-ter)',
-    50: 'var(--bulma-white-bis)',
-    0: 'var(--bulma-white)',
+    950: 'var(--bs-black)',
+    900: 'var(--bs-gray-900)',
+    800: 'var(--bs-gray-800)',
+    700: 'var(--bs-gray-700)',
+    600: 'var(--bs-gray-600)',
+    500: 'var(--bs-gray-500)',
+    400: 'var(--bs-gray-400)',
+    300: 'var(--bs-gray-300)',
+    200: 'var(--bs-gray-200)',
+    100: 'var(--bs-gray-100)',
+    50: '#fafbfc',
+    0: 'var(--bs-white)',
 }
 
 /***
@@ -35,60 +21,58 @@ const graySurfaceScheme = {
  */
 export const stylePreset = definePreset(Nora, {
     semantic: {
-        primary: bulmaPrimaryScheme,
         colorScheme: {
             light: {
                 surface: graySurfaceScheme,
-                text: 'var(--bulma-text)',
+                text: 'var(--bs-body-color)',
                 formField: {
-                    background: 'var(--bulma-scheme-main)',
-                    borderColor: 'var(--bulma-border)',
-                    disabledBackground: 'var(--bulma-white-ter)',
-                    disabledColor: 'var(--bulma-grey)',
+                    background: 'var(--bs-body-bg)',
+                    borderColor: 'var(--bs-border-color)',
+                    disabledBackground: 'var(--bulma-tertiary-bg)',
+                    disabledColor: 'var(--bulma-tertiary-color)',
                 },
                 content: {
-                    borderColor: 'var(--bulma-border)',
+                    borderColor: 'var(--bs-border-color)',
                 },
                 overlay: {
                     modal: {
-                        color: 'var(--bulma-text)',
+                        color: 'var(--bs-body-color)',
                     }
                 }
 
             },
             dark: {
                 surface: graySurfaceScheme,
-                text: 'var(--bulma-text)',
+                text: 'var(--bs-body-color)',
                 formField: {
-                    background: 'var(--bulma-scheme-main)',
-                    borderColor: 'var(--bulma-border)',
-                    disabledBackground: 'var(--bulma-black-ter)',
-                    disabledColor: 'var(--bulma-grey)',
-                    placeholderColor: 'var(--bulma-text-weak)'
+                    background: 'var(--bs-body-bg)',
+                    borderColor: 'var(--bs-border-color)',
+                    disabledBackground: 'var(--bs-tertiary-bg)',
+                    disabledColor: 'var(--bs-tertiary-color)',
+                    placeholderColor: 'var(--bs-secondary-color)'
                 },
                 content: {
-                    borderColor: 'var(--bulma-border)',
+                    borderColor: 'var(--bs-border-color)',
                 },
                 overlay: {
                     modal: {
-                        color: 'var(--bulma-text)',
+                        color: 'var(--bs-body-color)',
                     }
                 }
             }
         },
         formField: {
-            borderRadius: 'var(--bulma-radius)',
-            paddingY: 'calc(0.5em - var(--bulma-control-border-width))',
+            borderRadius: 'var(--bs-border-radius)',
         },
         content: {
-            borderRadius: 'var(--bulma-radius)',
+            borderRadius: 'var(--bs-border-radius)',
         },
         overlay: {
             select: {
-                borderRadius: 'var(--bulma-radius)',
+                borderRadius: 'var(--bs-border-radius)',
             },
             modal: {
-                borderRadius: 'var(--bulma-radius)',
+                borderRadius: 'var(--bs-border-radius)',
             }
         }
     }
