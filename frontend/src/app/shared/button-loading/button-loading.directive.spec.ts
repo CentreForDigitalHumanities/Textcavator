75b9fa74a462ced5811a97b9728a549f7bd2db12
Luka van der Plas <lukavdplas@mailfence.com>
2026-07-29 12:02:22 +0000

    improve event blocking in loading button

--- frontend/src/app/shared/button-loading/button-loading.directive.spec.ts
+++ frontend/src/app/shared/button-loading/button-loading.directive.spec.ts
@@ -5,7 +5,7 @@ import { ButtonLoadingDirective } from './button-loading.directive';
 
 @Component({
     template: `
-    <button class="btn btn-primary" [iaButtonLoading]="loading()">
+    <button class="btn btn-primary" [iaButtonLoading]="loading()" (click)="handleClick()">
         Test
     </button>
     `,
@@ -13,6 +13,8 @@ import { ButtonLoadingDirective } from './button-loading.directive';
 })
 class LoadingButtonTest {
     loading = signal(false);
+
+    handleClick() {}
 }
 
 describe('iaButtonLoading', () => {
@@ -45,5 +47,69 @@ describe('iaButtonLoading', () => {
         component.loading.set(false);
         fixture.detectChanges();
         expect(button.querySelector('.spinner-border')).toBeFalsy();
+    });
+
+    it('forwards click events', () => {
+        const spy = spyOn(component, 'handleClick');
+        expect(spy).not.toHaveBeenCalled()
+        button.click();
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('blocks click events while loading', () => {
+        component.loading.set(true);
+        fixture.detectChanges();
+        const spy = spyOn(component, 'handleClick');
+        button.click();
+        expect(spy).not.toHaveBeenCalled();
+    });
+});
+
+@Component({
+    template: `
+    <form (submit)="handleSubmit($event)">
+        <label>Test</label>
+        <input type="text">
+        <button class="btn btn-primary" type="submit" [iaButtonLoading]="loading()" >
+            Submit
+        </button>
+    </form>
+    `,
+    imports: [CommonModule, ButtonLoadingDirective],
+})
+class LoadingButtonFormTest {
+    loading = signal(false);
+
+    handleSubmit(event) {
+        event.preventDefault();
+    }
+}
+
+describe('iaButtonLoading', () => {
+    let fixture: ComponentFixture<LoadingButtonFormTest>;
+    let component: LoadingButtonFormTest;
+    let button: HTMLButtonElement;
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LoadingButtonFormTest);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        const element = fixture.debugElement.nativeElement as Element;
+        button = element.querySelector('button');
+    });
+
+    it('forwards default events', () => {
+        const spy = spyOn(component, 'handleSubmit').and.callThrough();
+        expect(spy).not.toHaveBeenCalled()
+        button.click();
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('prevents default events while loading', () => {
+        component.loading.set(true);
+        fixture.detectChanges();
+        const spy = spyOn(component, 'handleSubmit').and.callThrough();
+        button.click();
+        expect(spy).not.toHaveBeenCalled();
     });
 });
--- frontend/src/app/shared/button-loading/button-loading.directive.ts
+++ frontend/src/app/shared/button-loading/button-loading.directive.ts
@@ -1,17 +1,24 @@
-import { Directive, effect, ElementRef, inject, input } from '@angular/core';
+import { Directive, effect, ElementRef, inject, input, output } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { fromEvent } from 'rxjs';
 
 @Directive({
     selector: 'button[iaButtonLoading]',
     host: {
         // eslint-disable-next-line @typescript-eslint/naming-convention
-        '[disabled]': 'iaButtonLoading()'
+        '[attr.aria-disabled]': 'iaButtonLoading()',
     }
 })
 export class ButtonLoadingDirective {
     iaButtonLoading = input<boolean>(false);
+    forwardClick = output<Event>({ alias: 'click' });
     private el = inject(ElementRef);
 
     constructor() {
+        fromEvent(this.el.nativeElement, 'click', { capture: true }).pipe(
+            takeUntilDestroyed(),
+        ).subscribe(this.onClick.bind(this));
+
         effect(() => {
             if (this.iaButtonLoading()) {
                 this.showSpinner();
@@ -19,6 +26,16 @@ export class ButtonLoadingDirective {
                 this.hideSpinner();
             }
         })
+    }
+
+    onClick(event: Event) {
+        event.stopPropagation();
+        if (this.iaButtonLoading()) {
+            event.preventDefault();
+        } else {
+            this.forwardClick.emit(event);
+
+        }
     }
 
     showSpinner() {
