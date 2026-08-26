d38e6b979d119c5c113ffa2360ad6ad804269acd
Meesch <31687030+Meesch@users.noreply.github.com>
2026-08-26 16:40:23 +0200

    update title merged uk corpus

--- backend/corpora/parliament/uk-merged.py
+++ backend/corpora/parliament/uk-merged.py
@@ -4,7 +4,7 @@ from corpora.utils.constants import document_context
 
 
 class ParliamentUKMerged(ParliamentUK):
-    title = 'UK Parliament (P&P + TE)'
+    title = 'UK Parliamentary Debates'
     description = "Speeches from the House of Lords and House of Commons"
     min_date = datetime(year=1803, month=1, day=1)
     max_date = datetime(year=2025, month=12, day=31)
